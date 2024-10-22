35a3ba4c7d986b5ae13b2a14c2ed9bc1b2e37b6f
noblehunter <44024657+noblehunter@users.noreply.github.com>
2024-10-22 15:57:41 +0300

    Загрузка

--- Week5/Task1.a/src/main/java/edu/penzgtu/CatchExample.java
+++ Week5/Task1.a/src/main/java/edu/penzgtu/CatchExample.java
@@ -9,14 +9,34 @@ public class CatchExample {
         // Укажите путь к вашему текстовому файлу
         String filePath = "C:/Users/NobleHunter/Desktop/Projekt/HiLevelPrograming/Week5/Task1.a/src/main/java/edu/penzgtu/numbers.txt";
 
+        // Сначала мы прочитаем файл, чтобы узнать количество строк
+        int count = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            // Читаем файл построчно
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                count++;
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // Теперь создаем массив для хранения строк
+        String[] data = new String[count]; // Объявление массива // Снова читаем файл, чтобы заполнить массив
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int index = 0;
+            while ((line = br.readLine()) != null) {
+                // Сохраняем строку в массив
+                data[index] = line;
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Выводим массив на экран
+        for (String item : data) {
+            System.out.println(item);
         }
     }
 }
